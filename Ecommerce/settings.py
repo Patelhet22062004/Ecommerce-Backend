b7593f4d438e1535e06c99be05ee967525114149
Patel_Het <124024799+Patelhet22062004@users.noreply.github.com>
2025-05-02 15:06:02 +0530

    Update settings.py
    media change.

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -160,7 +160,7 @@ load_dotenv()
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
